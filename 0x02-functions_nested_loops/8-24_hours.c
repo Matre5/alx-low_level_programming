74b2496560b9285e232d5d3d722013b7c04f065d
Matre5 <aigmatre5@gmail.com>
2022-09-13 17:13:59 -0700

    Task

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *jack_bauer - Prints every minute of the day of
+ *             Jack Bauer, starting from 00:00 to 23:59.
+ */
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour <= 23; hour++)
+
+		for (minute = 0; minute <= 59; minute++)
+		{
+
+
