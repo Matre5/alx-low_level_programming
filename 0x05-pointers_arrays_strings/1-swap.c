97ce125b026495bd366a007ffe197945478f0579
Matre5 <aigmatre5@gmail.com>
2022-09-19 15:08:46 -0700

    Task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the value of two integers.
+ * @*a: The first integer
+ * @*b: The second integer
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @str: The string to get the length of.
+ *
+ * Return: The length of @str.
+ */
+int _strlen(const char *str)
+{
+	size_t length = 0;
+
+	while (*str++)
+		length ++;
+
+	return (length);
+}
