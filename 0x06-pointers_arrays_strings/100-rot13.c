b0bc43bb1d2ca9c7d7dfa5a5c96847075e37da86
Matre5 <aigmatre5@gmail.com>
2022-09-22 21:50:01 -0700

    Task

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+char *rot13(char *str)
+{
+	int indx1 = 0, indx2;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+		'G', 'H', 'I', 'J', 'K', 'L',
+		'M', 'N', 'O', 'P', 'Q', 'R',
+}
